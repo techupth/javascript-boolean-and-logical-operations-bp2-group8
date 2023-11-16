a2b4c9aab3759f68dde2f8bcd7d7a019a4ae2764
fonytas <fonyfonyta@gmail.com>
2023-11-16 14:42:30 +0700

    feat:Boolean and logical operations Assignments

--- ex-1.js
+++ ex-1.js
@@ -4,5 +4,9 @@ let hasCriminalBlacklist;
 let isAllow;
 
 // Start coding here
+// JamesInfo {age:18,CriminalBlacklist:No}
+isOver18 = true;
+hasCriminalBlacklist = false;
+isAllow = isOver18 && !hasCriminalBlacklist;
 
 console.log(isAllow);
