9d8634f3a85a680a6a3463888519242666f0733a
Krittin <rubruengyod@gmail.com>
2023-12-26 12:47:02 +0700

    Boolean and Logical Operation Assignment

--- ex-2.js
+++ ex-2.js
@@ -1,14 +1,11 @@
 // Exercise #2: Promotion Conditions
-
 // Start coding here
-// JohnInfo={lastMonthPaid:4001, buyWeekday:Friday, buyITProduct: No,
-//  eventAttend:2,Member:Gold}
 let lastMonthPaidMoreThan4000 = true;
 let isWeekday = true;
 let hasBoughtProductFromITCategory = false;
 let hasAttendedDiscountEvent = true;
 let isPlatinum = false;
 
-let hasPromotion= (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent) || isPlatinum;
+let hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && hasAttendedDiscountEvent && hasAttendedDiscountEvent) || isPlatinum;
 
 console.log(hasPromotion);
